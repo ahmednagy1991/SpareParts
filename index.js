019d734d0cffb952bc670108c43754b4409e42f0
Ahmed Nagy <ahmed_nagy1991@hotmail.com>
2018-12-30 11:31:57 -0800

    more test endpoints

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express=require('express');
 const app=express();
+
+
+const prt=process.env.PORT||3000;
+
 app.get('/test',(req,res)=>{
-    res.send("hello node");
+    res.send("woring on port : "+prt);
+});
+
+app.get('/api/getPart/:model/:year/:partType',(req,res)=>{
+    res.send(req.params);
+    //res.send(req.query);
+});
+
+app.get('/api/GetParts',(req,res)=>{
+    res.send([1,2,3]);
 });
 app.listen(3000,()=>{
-    console.log("listing to port 3000");
+    console.log(`listing to port ${prt}`);
 })
